491cb34e1eeba65448a22e632b6fc2705856bdb2
mskrishnamurali <mskrishnamurali1999@gmail.com>
2023-12-01 13:51:18 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import "./App.css"
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Todo from './ToDoApp/Todo'
+
+function App() {
+  return(
+<div className="App">
+<Todo/>
+
+</div>
+
+  )
+ 
+}
+export default App;
+
+
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Header = () => {
+  return (
+    <div>
+      <>
+      <h1>useState Hook in React</h1>
+      </>
+    </div>
+  )
+}
+
+export default Header
